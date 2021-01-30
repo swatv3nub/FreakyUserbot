709c9b01f58fcaedf1bbaf55e92f8388ce41c068
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2021-01-30 19:34:08 +0530

    Me Pagla gaya hu BC

--- HackfreaksUserbot/Configs.py
+++ HackfreaksUserbot/Configs.py
@@ -49,7 +49,7 @@ if ENV:
         # Send .get_id in any group with all your administration bots (added)
         G_BAN_LOGGER_GROUP = int(os.environ.get("G_BAN_LOGGER_GROUP", -1001198699233))
         # Tag Logger
-        TAG_LOG = int(os.environ.get("TAG_LOG", None))
+        TAG_LOG = int(os.environ.get("TAG_LOG", -100))
         # TG API limit. An album can have atmost 10 media!
         GOOGLE_SEARCH_COUNT_LIMIT = int(os.environ.get("GOOGLE_SEARCH_COUNT_LIMIT", 9))
         TG_GLOBAL_ALBUM_LIMIT = int(os.environ.get("TG_GLOBAL_ALBUM_LIMIT", 9))
