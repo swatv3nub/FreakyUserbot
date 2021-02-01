5396db3edf561192ff1144416f3ef6aee659c3d5
sᴡᴏɴɪᴛ <72299804+swatv3nub@users.noreply.github.com>
2021-02-01 17:00:32 +0530

    Update alive.py

--- FreakyUserbot/modules/alive.py
+++ FreakyUserbot/modules/alive.py
@@ -55,7 +55,7 @@ pm_caption += "**✗ License** : [GNU General Public License v3.0](https://githu
 pm_caption += "**[✗ Updates](https://t.me/FreakyUserbot)**\n"
 pm_caption += "**[✗ Support](https://t.meHackfreaksSupport)**\n\n"
 pm_caption += (
-    "**[✗ GitHub Respiratory ✗](https://github.com/swatv3nub/FreakyUserbot/)**\n"
+    "**[✗ GitHub Repository ✗](https://github.com/swatv3nub/FreakyUserbot/)**\n"
 )
 pm_caption += "**[✗ Deploy Freaky ✗](https://heroku.com/deploy?template=https://github.com/swatv3nub/FreakyUserbot)**"
 
