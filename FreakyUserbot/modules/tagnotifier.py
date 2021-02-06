f1984caed8a626baec8c3621dd8819e61c66b492
Swonit <swatv3nub@gmail.com>
2021-02-06 13:30:15 +0530

    Pokemon - Updated Tag Notifier
    Signed-off-by: Swonit <swatv3nub@gmail.com>

--- FreakyUserbot/modules/tagnotifier.py
+++ FreakyUserbot/modules/tagnotifier.py
@@ -1,61 +1,52 @@
 # (c) Shrimadhav U K
 #
-# From @Uniborg
+# This file is part of @UniBorg
 
-"""-_-
-"""
 from telethon import custom, events
-from telethon.tl.types import Channel
+from telethon.tl.types import Chat
 from telethon.utils import get_display_name
 
 from FreakyUserbot.Configs import Config
 
-if Config.TAG_LOG:
-    NEEDTOLOG = int(Config.TAG_LOG)
 
-if Config.TAG_LOG:
-
-    @Freaky.on(
-        events.NewMessage(
-            incoming=True,
-            blacklist_chats=Config.UB_BLACK_LIST_CHAT,
-            func=lambda e: (e.mentioned),
-        )
+@Freaky.on(
+    events.NewMessage(
+        incoming=True,
+        blacklist_chats=Config.UB_BLACK_LIST_CHAT,
+        func=lambda e: (e.mentioned),
     )
-    async def all_messages_catcher(event):
-        await event.forward_to(Var.TG_BOT_USER_NAME_BF_HER)
+)
+async def all_messages_catcher(event):
+    if Config.NEEDTOLOG == "DISABLE":
+        return
+    # the bot might not have the required access_hash to mention the appropriate PM
+    lol_spechide = await tgbot.get_me()
+    sedbruh = lol_spechide.username
+    await event.forward_to(sedbruh)
+    # construct message
+    # the message format is stolen from @MasterTagAlertBot
+    ammoca_message = ""
 
-        # the message format is stolen from @MasterTagAlertBot
-        ammoca_message = ""
+    who_ = await event.client.get_entity(event.sender_id)
+    if who_.bot or who_.verified or who_.support:
+        return
 
-        who_ = await event.client.get_entity(event.sender_id)
-        if who_.bot or who_.verified or who_.support:
-            return
+    who_m = f"[{get_display_name(who_)}](tg://user?id={who_.id})"
 
-        who_m = f"[{get_display_name(who_)}](tg://user?id={who_.id})"
+    where_ = await event.client.get_entity(event.chat_id)
 
-        where_ = await event.client.get_entity(event.chat_id)
+    where_m = get_display_name(where_)
+    button_text = "✗ Go to Message ✗"
 
-        where_m = get_display_name(where_)
-        button_text = "✗ Go to Message ✗"
+    if isinstance(where_, Chat):
+        message_link = f"https://t.me/c/{where_.id}/{event.id}"
+    else:
 
-        if isinstance(where_, Channel):
-            message_link = f"https://t.me/c/{where_.id}/{event.id}"
-        else:
-            message_link = f"tg://openmessage?chat_id={where_.id}&message_id={event.id}"
-
-        ammoca_message += f"{who_m} tagged you in [{where_m}]({message_link})"
-        if NEEDTOLOG is not None:
-            await Freaky.send_message(
-                entity=NEEDTOLOG,
-                message=ammoca_message,
-                link_preview=False,
-                buttons=[[custom.Button.url(button_text, message_link)]],
-                silent=True,
-            )
-        else:
-            return
-
-
-# Tag notifier plugin, to get notifications if someone tags you.
-# Set TAG_LOG if you want this plugin to work.
+    ammoca_message += f"User {who_m} Have Tagged You Here -> [{where_m}]({message_link}) \nCheck Message 👇 "
+    log_chat = Config.TAG_LOG
+    await tgbot.send_message(
+        log_chat,
+        message=ammoca_message,
+        link_preview=False,
+        buttons=[[custom.Button.url(button_text, message_link)]],
+    )
