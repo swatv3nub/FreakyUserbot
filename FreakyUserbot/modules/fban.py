5502aba3784b5edd47cfb3146aa27123f5e3f3e4
Swonit <swatv3nub@gmail.com>
2021-02-08 16:35:47 +0530

    Update Fban
    Signed-off-by: Swonit <swatv3nub@gmail.com>

--- FreakyUserbot/modules/fban.py
+++ FreakyUserbot/modules/fban.py
@@ -14,42 +14,34 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-"""Commands Available
-.fadd <fedid> = Add Fed in DB
-.fadd all = Add All feds in DB
-.frm <fedid> = Remove fed from DB
-.frm all = Remove All Feds from DB
-.fban <id/username> <reason> = Fed Ban the Targetted User in the Feds in DB
-.unfban <id/username> = UnFedBan the Targetted user in the Feds in DB
-"""
-
-import asyncio
-
-from FreakyUserbot.functions import fetch_feds
+from FreakyUserbot.function import fetch_feds
 from FreakyUserbot.modules.sql_helper.feds_sql import (
     add_fed,
     get_all_feds,
     is_fed_indb,
     rmfed,
 )
+import asyncio
 from FreakyUserbot.utils import Freaky_on_cmd
-
+from FreakyUserbot import CMD_HELP
 chnnl_grp = Config.FBAN_GROUP
 
 
 @Freaky.on(Freaky_on_cmd(pattern="fadd ?(.*)"))
 async def _(event):
+    if event.fwd_from:
+        return
     nolol = 0
     yeslol = 0
     await event.edit("`Processing..`")
-    freakz = event.pattern_match.group(1)
-    if freakz == None:
+    lol_s = event.pattern_match.group(1)
+    if lol_s == None:
         await event.edit("`Give FeD ID`")
         return
-    elif freakz == " ":
+    elif lol_s == " ":
         await event.edit("`Give FeD ID`")
         return
-    elif freakz == "all":
+    elif lol_s == "all":
         hmm = await fetch_feds(event, borg)
         for i in hmm:
             if is_fed_indb(i):
@@ -58,79 +50,66 @@ async def _(event):
                 add_fed(i)
                 yeslol += 1
         await event.edit(f"`Added {yeslol} Feds To DB, Failed To Add {nolol} Feds.`")
-    elif is_fed_indb(freakz):
+    elif is_fed_indb(lol_s):
         await event.edit("`Fed Already Found On DataBase.`")
         return
-    elif not is_fed_indb(freakz):
-        add_fed(freakz)
+    elif not is_fed_indb(lol_s):
+        add_fed(lol_s)
         await event.edit("`Done ! Added This Fed To DataBase`")
 
 
 @Freaky.on(Freaky_on_cmd(pattern="frm ?(.*)"))
 async def _(event):
-    freakz = event.pattern_match.group(1)
+    if event.fwd_from:
+        return
+    lol_s = event.pattern_match.group(1)
     await event.edit("`Processing..`")
     lol = get_all_feds()
-    if freakz == None:
+    if lol_s == None:
         await event.edit("`Give FeD ID`")
         return
-    elif freakz == " ":
+    elif lol_s == " ":
         await event.edit("`Give FeD ID`")
         return
-    elif freakz == "all":
+    elif lol_s == "all":
         for sedm in lol:
             rmfed(sedm.feds)
         await event.edit("`Done, Cleared. All Fed Database.`")
-    elif is_fed_indb(freakz):
-        rmfed(freakz)
+    elif is_fed_indb(lol_s):
+        rmfed(lol_s)
         await event.edit("`Done, Removed This FeD From DB`")
-    elif not is_fed_indb(freakz):
+    elif not is_fed_indb(lol_s):
         await event.edit("`This Fed Not Found On Db.`")
 
 
-@Freaky.on(Freaky_on_cmd(pattern="fban ?(.*)"))
+@Freaky.on(Freaky_on_cmd(pattern="fban"))
 async def _(event):
-    await event.edit("Starting Fed Bans...")
-    arg = event.text.split(" ", maxsplit=2)
-    if len(arg) > 2:
-        FBAN = arg[1]
-        REASON = arg[2]
-    else:
-        FBAN = arg[1]
-        REASON = " MassBanned Kela #FreakyOP."
-
-    try:
-        int(FBAN)
-        if int(FBAN) == "1228116248" or int(FBAN) == "1167145475":
-            await event.edit("Error, Can't Fban Dev.")
-            return
-    except BaseException:
-        if FBAN == "@TheFuckErGuy" or FBAN == "@Swonit":
-            await event.edit("Error, can't Fban Dev.")
-            return
-    else:
-        await event.edit("`Something Went Wrong.`")
+    if event.fwd_from:
+        return
+    lol_s = event.text.split(" ", maxsplit=1)[1]
+    if lol_s == None:
+        await event.edit("`No user Found To Fban.`")
         return
     all_fed = get_all_feds()
     errors = 0
     len_feds = len(all_fed)
     if len_feds == 0:
-        await event.edit(
-            "`No Fed IN DB, Add One To Do So. Please Do .fadd all by Tagging your FedAdmin File to Add All Feds IN Database`"
-        )
+        await event.edit("`No Fed IN DB, Add One To Do So. Please Do .fadd all to Add All Feds IN Database`")
         return
     await event.edit(f"`FBanning in {len_feds} Feds.`")
-
+    if not chnnl_grp:
+        await event.edit("Bruh, Atleast Set Fban Group Var, Do `.set var FBAN_GROUP <yourgroupidhere>`")
+        return
     try:
         await borg.send_message(chnnl_grp, "/start")
     except Exception as e:
         await event.edit("**Errors** : " + str(e))
         return
-    for kela in all_fed:
+    for teamz in all_fed:
         try:
-            await borg.send_message(chnnl_grp, "/joinfed " + kela.feds)
+            await borg.send_message(chnnl_grp, "/joinfed " + teamz.feds)
             await asyncio.sleep(2)
-            await borg.send_message(chnnl_grp, "/fban " + {FBAN} + {REASON})
+            await borg.send_message(chnnl_grp, "/fban " + lol_s)
             await asyncio.sleep(5)
         except:
             errors += 1
@@ -141,27 +120,51 @@ async def _(event):
 
 @Freaky.on(Freaky_on_cmd(pattern="unfban ?(.*)"))
 async def _(event):
-    freakz = event.pattern_match.group(1)
-    if freakz == None:
+    if event.fwd_from:
+        return
+    lol_s = event.pattern_match.group(1)
+    if lol_s == None:
         await event.edit("`No User Found To Fban.`")
         return
     all_fed = get_all_feds()
     errors = 0
     len_feds = len(all_fed)
     await event.edit(f"`UnFBanning in {len_feds} Feds.`")
+    if not chnnl_grp:
+        await event.edit("Bruh, Atleast Set Fban Group Var, Do `.set var FBAN_GROUP <yourgroupidhere>`")
+        return
     try:
         await borg.send_message(chnnl_grp, "/start")
     except Exception as e:
         await event.edit("**Errors** : " + str(e))
         return
-    for kela in all_fed:
+    for teamz in all_fed:
         try:
-            await borg.send_message(chnnl_grp, "/joinfed " + kela.feds)
+            await borg.send_message(chnnl_grp, "/joinfed " + teamz.feds)
             await asyncio.sleep(2)
-            await borg.send_message(chnnl_grp, "/unfban " + freakz)
+            await borg.send_message(chnnl_grp, "/unfban " + lol_s)
             await asyncio.sleep(5)
         except:
             errors += 1
     await event.edit(
         f"**Un-Fban Completed** \nTotal Sucess : `{len_feds - errors}` \nTotal Errors : `{errors}` \nTotal Fed Len : `{len_feds}`"
     )
+
+
+CMD_HELP.update(
+    {
+        "fed_ban": "**Fed Ban**\
+\n\n**Syntax : **`.fadd <fed-id>`\
+\n**Usage :** Adds The Given Fed In Fban database.\
+\n\n**Syntax : **`.fadd all`\
+\n**Usage :** Adds All The Feds In The Database Where You Are Admin.\
+\n\n**Syntax : **`.frm <fed-id>`\
+\n**Usage :** Removes The Given Fed From The Fban database.\
+\n**Example :** `.frm add`\
+\n**Note** :** Removes All The Feds From The Database.\
+\n**Example :** `.fban <username/User-ID>`\
+\n**Note** :** FBans The User.\
+\n**Example :** `.unfban <username/User-ID>`\
+\n**Note** :** UnFbans The User."
+    }
+)
